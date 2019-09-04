91b67926f2691d1ee82d8f96f25af6d3857b0345
xfali <damao1222@live.com>
2019-09-04 09:37:38 +0800

    add log

---
+++ test/log_test.go
@@ -0,0 +1,17 @@
+// Copyright (C) 2019, Xiongfa Li.
+// All right reserved.
+// @author xiongfa.li
+// @version V1.0
+// Description: 
+
+package test
+
+import (
+    "github.com/xfali/goutils/log"
+    "testing"
+)
+
+func TestLog(t *testing.T) {
+    log.Info("123")
+    //log.Fatal("exit")
+}
