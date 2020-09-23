33c8375d78bd69c1273c4b4b4b04a59a47bd8ab0
xfali <damao1222@live.com>
2020-09-23 23:12:49 +0800

    修改recycleMap

--- container/recycleMap/recycle_map.go
+++ container/recycleMap/recycle_map.go
@@ -9,54 +9,102 @@
 package recycleMap
 
 import (
+	"math"
 	"runtime"
 	"sync"
 	"time"
 )
 
+const (
+	// 默认清理时间间隔
+	DefaultPurgeInterval = 50 * time.Millisecond
+	// 默认每次清理个数
+	DefaultPurgeNumberPerTime = math.MaxInt64
+)
+
+type RecycleMap interface {
+	//设置一个值，含过期时间
+	Set(key, value interface{}, expireIn time.Duration) error
+
+	//根据key获取value
+	Get(key interface{}) interface{}
+
+	Delete(key interface{})
+
+	//根据key设置key过期时间
+	SetExpire(key interface{}, expireIn time.Duration) bool
+
+	//获得key过期时间
+	TTL(key interface{}) time.Duration
+
+	//获得总数
+	Size() int64
+
+	Close() error
+}
+
 type dataEntity struct {
 	value      interface{}
-	expireTime time.Duration
+	expireTime time.Time
 }
 
-type RecycleMap struct {
-	PurgeInterval time.Duration
-	db            map[interface{}]dataEntity
-	stop          chan bool
-	Lock          sync.Locker
+type defaultRecycleMap struct {
+	purgeInterval time.Duration
+	purgeNumber   int64
+
+	db   map[interface{}]*dataEntity
+	stop chan bool
+	lock sync.Locker
 }
 
-func New() *RecycleMap {
-	ret := &RecycleMap{
-		db:   map[interface{}]dataEntity{},
-		stop: make(chan bool),
-		Lock: &sync.Mutex{},
+type Opt func(*defaultRecycleMap)
+
+func New(opts ...Opt) RecycleMap {
+	ret := &defaultRecycleMap{
+		purgeInterval: DefaultPurgeInterval,
+		purgeNumber:   DefaultPurgeNumberPerTime,
+		db:            map[interface{}]*dataEntity{},
+		stop:          make(chan bool),
+		lock:          &sync.Mutex{},
+	}
+	for _, opt := range opts {
+		opt(ret)
 	}
 
+	ret.run()
 	return ret
 }
 
-func (dm *RecycleMap) purge() {
-	dm.Lock.Lock()
-	defer dm.Lock.Unlock()
+func (dm *defaultRecycleMap) purge() {
+	dm.lock.Lock()
+	defer dm.lock.Unlock()
 
-	now := time.Duration(time.Now().UnixNano())
+	now := time.Now()
+	var i int64 = 0
 	for k, v := range dm.db {
-		if v.expireTime <= now {
+		if v.expireTime.IsZero() {
+			continue
+		}
+		if !v.expireTime.After(now) {
 			delete(dm.db, k)
+			i++
+			if i >= dm.purgeNumber {
+				return
+			}
 		}
 	}
 }
 
 //初始化并开启回收线程，必须调用
-func (dm *RecycleMap) Run() {
-	if dm.PurgeInterval <= 0 {
-		dm.PurgeInterval = 0
+func (dm *defaultRecycleMap) run() {
+	if dm.purgeInterval <= 0 {
+		dm.purgeInterval = 0
 	}
 
 	go func() {
-		if dm.PurgeInterval > 0 {
-			timer := time.NewTicker(dm.PurgeInterval)
+		if dm.purgeInterval > 0 {
+			timer := time.NewTicker(dm.purgeInterval)
+			defer timer.Stop()
 			for {
 				select {
 				case <-dm.stop:
@@ -71,7 +119,6 @@ func (dm *RecycleMap) Run() {
 				case <-dm.stop:
 					return
 				default:
-
 				}
 				dm.purge()
 
@@ -82,74 +129,132 @@ func (dm *RecycleMap) Run() {
 }
 
 //关闭
-func (dm *RecycleMap) Close() {
+func (dm *defaultRecycleMap) Close() error {
 	close(dm.stop)
+	return nil
 }
 
-//设置一个值，含过期时间
-func (dm *RecycleMap) Set(key, value interface{}, expireIn time.Duration) {
-	dm.Lock.Lock()
-	defer dm.Lock.Unlock()
+// 设置一个值，含过期时间
+// 如果expireIn设置为-1，则永不过期
+func (dm *defaultRecycleMap) Set(key, value interface{}, expireIn time.Duration) error {
+	dm.lock.Lock()
+	defer dm.lock.Unlock()
 
-	time := expireIn + time.Duration(time.Now().UnixNano())
-	dm.db[key] = dataEntity{value: value, expireTime: time}
+	if expireIn >= 0 {
+		dm.db[key] = &dataEntity{value: value, expireTime: time.Now().Add(expireIn)}
+	} else {
+		dm.db[key] = &dataEntity{value: value}
+	}
+
+	return nil
 }
 
 //根据key获取value
-func (dm *RecycleMap) Get(key interface{}) interface{} {
-	dm.Lock.Lock()
-	defer dm.Lock.Unlock()
+func (dm *defaultRecycleMap) Get(key interface{}) interface{} {
+	dm.lock.Lock()
+	defer dm.lock.Unlock()
 
 	v, ok := dm.db[key]
 	if ok {
+		// expired
+		if v.expireTime.IsZero() {
+			return v.value
+		}
+		now := time.Now()
+		if !v.expireTime.After(now) {
+			return nil
+		}
 		return v.value
 	} else {
 		return nil
 	}
 }
 
+//获得总数
+func (dm *defaultRecycleMap) Size() int64 {
+	dm.lock.Lock()
+	dm.lock.Unlock()
+
+	var size int64 = 0
+	now := time.Now()
+	for _, v := range dm.db {
+		if v.expireTime.IsZero() {
+			size++
+			continue
+		}
+		if !v.expireTime.After(now) {
+		} else {
+			size++
+		}
+	}
+	return size
+}
+
 //删除key
-func (dm *RecycleMap) Del(key interface{}) {
-	dm.Lock.Lock()
-	defer dm.Lock.Unlock()
+func (dm *defaultRecycleMap) Delete(key interface{}) {
+	dm.lock.Lock()
+	defer dm.lock.Unlock()
 	delete(dm.db, key)
 }
 
 //根据key设置key过期时间
-func (dm *RecycleMap) SetExpire(key interface{}, expireIn time.Duration) bool {
-	dm.Lock.Lock()
-	defer dm.Lock.Unlock()
+func (dm *defaultRecycleMap) SetExpire(key interface{}, expireIn time.Duration) bool {
+	dm.lock.Lock()
+	defer dm.lock.Unlock()
 
 	v, ok := dm.db[key]
 	if ok {
-		v.expireTime = expireIn + time.Duration(time.Now().UnixNano())
+		if expireIn >= 0 {
+			v.expireTime = time.Now().Add(expireIn)
+		} else {
+			v.expireTime = time.Time{}
+		}
 		return true
 	} else {
 		return false
 	}
 }
 
-//获得key过期时间
-func (dm *RecycleMap) TTL(key interface{}) time.Duration {
-	dm.Lock.Lock()
-	defer dm.Lock.Unlock()
+//获得key过期时间, 如果不存在返回-2，如果永不过期返回-1
+func (dm *defaultRecycleMap) TTL(key interface{}) time.Duration {
+	dm.lock.Lock()
+	defer dm.lock.Unlock()
 
 	v, ok := dm.db[key]
 	if ok {
-		return v.expireTime - time.Duration(time.Now().UnixNano())
+		if v.expireTime.IsZero() {
+			return -1
+		}
+		return v.expireTime.Sub(time.Now())
 	} else {
-		return -1
+		return -2
+	}
+}
+
+// 配置清理时间间隔（默认50ms）
+// 设置时间间隔越短清理得越及时，但是消耗更多CPU
+// 设置时间间隔越长内存消耗越多。
+func OptSetPurgeInterval(interval time.Duration) Opt {
+	return func(recycleMap *defaultRecycleMap) {
+		recycleMap.purgeInterval = interval
+	}
+}
+
+// 配置每次清理的清理数量，如果超出则放到下次清理(默认全部清理)
+func OptSetPurgeNumberPerTime(number int64) Opt {
+	return func(recycleMap *defaultRecycleMap) {
+		recycleMap.purgeNumber = number
 	}
 }
 
 //开启事务
-func (dm *RecycleMap) Multi() error {
+func (dm *defaultRecycleMap) Multi() error {
 	//dm.Lock.Lock()
 	return nil
 }
 
 //执行事务
-func (dm *RecycleMap) Exec() error {
+func (dm *defaultRecycleMap) Exec() error {
 	//dm.Lock.Unlock()
 	return nil
 }
--- test/recyclemap_test.go
+++ test/recyclemap_test.go
@@ -16,20 +16,76 @@ import (
 
 func TestRecycleMap(t *testing.T) {
 	dm := recycleMap.New()
-	dm.Run()
+	defer dm.Close()
 
-	dm.Set("123", "456", time.Second)
+	dm.Set("123", "abc", 50*time.Millisecond)
+	dm.Set("456", "efg", -1)
+	dm.Set("789", "hij", -1)
 
 	v1 := dm.Get("123")
-	t.Logf("value is %v\n", v1)
+	if v1.(string) != "abc" {
+		t.Fatal("must be abc")
+	}
+	t.Logf("value is %v, ttl: %d\n", v1, dm.TTL("123")/time.Millisecond)
+	size := dm.Size()
+	if size != 3 {
+		t.Fatal("expect 3 but get ", size)
+	}
 
-	time.Sleep(time.Second)
+	time.Sleep(50 * time.Millisecond)
 
 	v2 := dm.Get("123")
+	if v2 != nil {
+		t.Fatalf("v2 must be nil, %d\n", dm.TTL("123")/time.Millisecond)
+	}
 	t.Logf("After 1 second value is %v\n", v2)
+	size = dm.Size()
+	if size != 2 {
+		t.Fatal("expect 3 but get ", size)
+	}
 
 	time.Sleep(time.Millisecond)
 
 	v3 := dm.Get("123")
+	if v3 != nil {
+		t.Fatalf("v2 must be nil, %d\n", dm.TTL("123")/time.Millisecond)
+	}
 	t.Logf("After 1 second 1 Millisecond value is %v\n", v3)
+
+	v4 := dm.Get("456")
+	if v4.(string) != "efg" {
+		t.Fatal("must be efg")
+	}
+	t.Log(v4, dm.TTL("456"))
+
+	dm.SetExpire("456", 50*time.Millisecond)
+	t.Log(dm.TTL("456"))
+	time.Sleep(50 * time.Millisecond)
+	v5 := dm.Get("456")
+	if v5 != nil {
+		t.Fatal("must be nil")
+	}
+	size = dm.Size()
+	if size != 1 {
+		t.Fatal("expect 1 but get ", size)
+	}
+
+	v6 := dm.Get("789")
+	if v6.(string) != "hij" {
+		t.Fatal("must be hij")
+	} else {
+		t.Log(v6)
+	}
+
+	dm.Delete("789")
+	if dm.Get("789") != nil {
+		t.Fatal("must be nil")
+	}
+
+	time.Sleep(50*time.Millisecond)
+	size = dm.Size()
+	if size != 0 {
+		t.Fatal("expect 3 but get ", size)
+	}
+
 }
